eba077743d67f308a879d6c020c8723c674d6ccb
Shouvick <deeprony7@gmail.com>
2021-04-02 01:29:34 +0530

    added item styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import { useQuery } from 'react-query'
+import { useState } from "react";
+import { useQuery } from "react-query";
 // Components
-import Item from './Item/item'
-import Drawer from '@material-ui/core/Drawer'
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Grid from '@material-ui/core/Grid'
-import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
-import Badge from '@material-ui/core/Badge'
+import Item from "./Item/item";
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 // Styles
-import {Wrapper} from './App.styles';
+import { Wrapper } from "./App.styles";
 // Types
 export type CartItemType = {
   id: number;
@@ -18,39 +18,38 @@ export type CartItemType = {
   price: number;
   title: string;
   amount: number;
-}
+};
 
-
-
-const getProducts = async (): Promise<CartItemType[]> => 
-  await (await fetch('https://fakestoreapi.com/products')).json();
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
-  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
-  console.log( isLoading,data)
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(isLoading, data);
 
   const getTotalItems = () => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
-   
+
   const handleRemoveFromCart = () => null;
 
-  if (isLoading) return <LinearProgress/>
-  if (error) return <div>Something went wrong...</div>
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
 
   return (
     <Wrapper>
       <Grid container spacing={3}>
-        {data?.map(item => (
+        {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
-            <Item
-              item={item}
-              handleAddToCart={handleAddToCart} />
+            <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
         ))}
       </Grid>
     </Wrapper>
   );
-}
+};
 
 export default App;
