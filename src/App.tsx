f0d9a4facceb072f520afe110eca129d93c135ac
Shouvick <deeprony7@gmail.com>
2021-04-02 23:45:53 +0530

    added cart and cart item component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 // Components
 import Item from "./Item/item";
+import Cart from "./Cart/Cart"
 import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
@@ -33,7 +34,8 @@ const App = () => {
   );
   console.log(isLoading, data);
 
-  const getTotalItems = (items: CartItemType[]) => null;
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((acc: number, item) => acc + item.amount, 0)
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -44,17 +46,20 @@ const App = () => {
 
   return (
     <Wrapper>
-      <Drawer 
-        anchor='right' 
+      <Drawer
+        anchor='right'
         open={cartOpen}
         onClose={() => setCartOpen(false)}>
-          Cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart} />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)}
           color='error'>
-            <AddShoppingCartIcon/>
-          </Badge>
+          <AddShoppingCartIcon />
+        </Badge>
       </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,9 @@
+import Button from '@material-ui/core/Button'
+// Types
+import { CartItemType } from '../App'
+//Styles
+import { Wrapper } from './CartItem.styles'
+
+const CartItem: React.FC = () => <div>CartItem</div>
+
+export default CartItem;
